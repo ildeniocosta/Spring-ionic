20d609a3fc40c7681ef677cff558890d46a9328c
ildenio <ildeniocosta@gmail.com>
2019-07-23 16:48:39 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
 import { AuthService } from '../../services/auth.service';
 
@@ -9,29 +10,42 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
   creds : CredenciaisDTO = {
-    email : "",
-    senha : ""
+    email: "",
+    senha: ""
   };
-  constructor(public navCtrl: NavController, 
-              public menu: MenuController,
-              public auth: AuthService) {
+
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
-  ionViewWillEnter(){ 
+
+  ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
-  ionViewDidLeave(){
+    
+  ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
-  login(){
-    this.auth.authenticate(this.creds)
-    .subscribe(response => {
-      this.auth.successfulLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriasPage');
-    }),
-    error => {}
-    
-    }
 
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});  
+  }
+
+  login() {
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
+  }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from "@angular/core";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
-import { StorageService } from "./storage.service";
 import { LocalUser } from "../models/local_user";
-import {JwtHelper} from "angular2-jwt";
-
+import { StorageService } from "./storage.service";
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
-export class AuthService{
-    jwtHelper: JwtHelper = new JwtHelper();
-    
-    constructor(public http: HttpClient,
-                public storage: StorageService){
+export class AuthService {
 
+    jwtHelper: JwtHelper = new JwtHelper();
+
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
+
     authenticate(creds : CredenciaisDTO) {
         return this.http.post(
             `${API_CONFIG.baseUrl}/login`, 
@@ -24,12 +23,22 @@ export class AuthService{
                 responseType: 'text'
             });
     }
+
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }
+
     successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
             token: tok,
             email: this.jwtHelper.decodeToken(tok).sub
-            
         };
         this.storage.setLocalUser(user);
     }
@@ -38,5 +47,3 @@ export class AuthService{
         this.storage.setLocalUser(null);
     }
 }
-        
-    
